5946484bbda064f0079b0cb29271e2062a7ebf38
Kshitij Chaurasiya <kshitij.chaurasiya@gmail.com>
2024-12-21 18:44:51 +0530

    feat: optimize bubble sort and add selection sort.

--- sort/bubble.go
+++ sort/bubble.go
@@ -1,7 +1,9 @@
 package sort
 
 /*
-Bubble Sort is a simple comparison-based sorting algorithm. It repeatedly steps through the list, compares adjacent elements, and swaps them if they are in the wrong order. This process continues until no more swaps are needed, indicating that the list is sorted.
+Bubble Sort is a simple comparison-based sorting algorithm.
+It repeatedly steps through the list, compares adjacent elements, and swaps them if they are in the wrong order.
+This process continues until no more swaps are needed, indicating that the list is sorted.
 
 How it Works:
 - Compare adjacent elements in the list.
@@ -9,13 +11,20 @@ How it Works:
 - Repeat the process for the entire list, moving the largest unsorted element to its correct position with each pass.
 - Continue until the entire list is sorted.
 */
-func Bubble[T any](nums []T, compare ComparableFunc[T]) {
-	n := len(nums)
-	for i := 0; i < n; i++ {
+func Bubble[T any](elements []T, compare ComparableFunc[T]) {
+	n := len(elements)
+	for i := 0; i < n-1; i++ {
+		swapped := false
+		// Perform one pass of bubble sort
 		for j := 0; j < n-i-1; j++ {
-			if compare(nums[j], nums[j+1]) {
-				nums[j], nums[j+1] = nums[j+1], nums[j]
+			if compare(elements[j], elements[j+1]) {
+				elements[j], elements[j+1] = elements[j+1], elements[j]
+				swapped = true
 			}
+		}
+		// If no elements were swapped, the list is sorted
+		if !swapped {
+			break
 		}
 	}
 }
--- sort/selection.go
+++ sort/selection.go
@@ -1,1 +1,30 @@
 package sort
+
+/*
+Selection Sort is a comparison-based sorting algorithm that divides the input list into two parts:
+a sorted portion and an unsorted portion.
+The algorithm repeatedly selects the smallest (or largest, depending on the order)
+element from the unsorted portion and swaps it with the first element in the unsorted portion,
+moving it into the sorted portion.
+
+How Selection Sort Works:
+Start with the entire list as unsorted.
+Find the smallest element in the unsorted portion of the list.
+Swap it with the first element of the unsorted portion.
+Move the boundary between the sorted and unsorted portions by one element.
+Repeat steps 2–4 until the entire list is sorted.
+*/
+func Selection[T any](elements []T, compare ComparableFunc[T]) {
+	n := len(elements)
+	for i := 0; i < n; i++ {
+		temp := elements[i]
+		temp_ind := i
+		for j := i; j < n; j++ {
+			if compare(elements[j], temp) {
+				temp = elements[j]
+				temp_ind = j
+			}
+		}
+		elements[i], elements[temp_ind] = elements[temp_ind], elements[i]
+	}
+}
